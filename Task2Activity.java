bb4622acfd5664f9a49774bf03bdd64c2aaffcd6
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-05-02 07:25:47 +0000

    Task 2 done print "H3110 w0r1d 2.0 true"

---
+++ Task2Activity.java
@@ -0,0 +1,13 @@
+class Task2Activity{
+    public static void main(String[] args) {
+        byte b1 = 3;
+        short s1 = 1;
+        int i2 = 0;
+        char c2 = ' ';
+        float f1 = 2.0f;
+        boolean bool = true;
+
+        String mes = "H" + b1 + s1 + s1 + i2 + c2 + "w" + i2 + "r" +s1 +"d " + f1 + c2 + bool;
+        System.out.println(mes);
+    }
+}
